542cfbda12e8617a6658d4d133630729c9cfd184
ryoo <ryooc072112@gmail.com>
2023-05-02 14:39:58 +0900

    ✨ create repository

---
+++ src/pages/components/todoList.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+import { FC, Dispatch, SetStateAction } from "react";
+import { TaskType } from "..";
+
+type stateType = {
+  taskList: TaskType[];
+  setTaskList: Dispatch<SetStateAction<TaskType[]>>;
+};
+
+export const TodoList: FC<stateType> = ({ taskList, setTaskList }) => {
+  const handleDone = (id: number) => {
+    setTaskList(
+      taskList.map((task) => {
+        if (id === task.id) {
+          return {
+            ...task,
+            completed: !task.completed,
+          };
+        }
+        return task;
+      })
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTaskList(taskList.filter((task) => task.id !== id));
+  };
+  return (
+    <div className={styles.todoList}>
+      <div className={styles.flex}>
+        {taskList.map((task, index) => (
+          <div
+            key={index}
+            className={`${styles.flex1} ${
+              task.completed ? styles.completed : ""
+            }`}
+          >
+            <div>
+              <ul>
+                <li>
+                  <span>{task.text}</span>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <button type="button" onClick={() => handleDone(task.id)}>
+                <Image alt="check" src="/check.png" width={25} height={25} />
+              </button>
+              <button type="button" onClick={() => handleDelete(task.id)}>
+                <Image alt="trash" src="/trash.png" width={25} height={25} />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/components/todoPage.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+import { FC, useState, Dispatch, SetStateAction } from "react";
+import { TaskType } from "..";
+
+type submitType = React.FormEvent<HTMLFormElement>;
+type changeType = React.ChangeEvent<HTMLInputElement>;
+type clickType = React.MouseEvent<HTMLButtonElement, MouseEvent>;
+type stateType = {
+  taskList: TaskType[];
+  setTaskList: Dispatch<SetStateAction<TaskType[]>>;
+};
+
+export const MainPage: FC<stateType> = ({ taskList, setTaskList }) => {
+  const [inputText, setInputText] = useState("");
+  const addTask = (e: submitType | clickType) => {
+    e.preventDefault();
+    if (inputText == "") {
+      return;
+    }
+    setTaskList([
+      ...taskList,
+      {
+        id: taskList.length,
+        text: inputText,
+        completed: false,
+      },
+    ]);
+    setInputText("");
+    console.log(taskList);
+  };
+
+  const handleChange = (e: changeType) => {
+    setInputText(e.target.value);
+  };
+  return (
+    <div className={styles.mainPage}>
+      <h3>Your personal productivity tool</h3>
+
+      <form onSubmit={(e: submitType) => addTask(e)}>
+        <input
+          type="text"
+          onChange={(e: changeType) => handleChange(e)}
+          placeholder="What is your todo?"
+          value={inputText}
+        />
+        <button onClick={(e: clickType) => addTask(e)} type="submit">
+          <Image alt="completed" src="/plus.png" width={30} height={30} />
+        </button>
+      </form>
+      <h4>[ To Do List ]</h4>
+    </div>
+  );
+};
